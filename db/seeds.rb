62f166f1303ad119dbaecbfac9eb52d9842cccca
Tomas <tomasperezg@gmail.com>
2023-08-09 20:04:29 -0400

    Add fake data generation with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Crear 20 clientes
+20.times do
+    Customer.create(name: Faker::Name.name)
+  end
+
+puts "Creando 20 clientes"
+  
+  # Crear 100 películas
+  100.times do
+    Movie.create(name: Faker::Movie.title, description: Faker::Movie.quote, customer_id: Customer.order(Arel.sql('RANDOM()')).first.id)
+  end
+
+puts "Creando 100 peliculas"
